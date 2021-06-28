8a032eb10dfb3c7202f2fb548799b5d810adf082
Nechaev101 <nechaev.kv@r1pro.ru>
2021-06-28 14:38:21 +0500

    setup

---
+++ AutomaticMEPTrace/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace AutomaticMEPTrace
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
